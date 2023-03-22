9772fa538d3b6209b58e49c97114f1006ea5ca47
Jankes <jankes@jankes.com.pl>
2023-03-22 20:39:00 +0100

    Copied Request class from Codeholic's MVC Framework

---
+++ app/Core/Request.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+class Request
+{
+    private array $routeParams = [];
+
+    public function getUrl()
+    {
+        $path = $_SERVER['REQUEST_URI'] ?? "/";
+        $position = strpos($path, '?'); //pozycja znaku zapytania 
+        if ($position !== false) {
+            $path = substr($path, 0, $position); 
+        }
+        return $path;
+    }
+
+    public function getMethod()
+    {
+        //return strtolower($_SERVER['REQUEST_METHOD']);
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    public function isGet()
+    {
+        return $this->getMethod() === 'get';
+    }
+
+    public function isPost()
+    {
+        return $this->getMethod() === 'post';
+    }
+
+    public function getBody()
+    {
+        $body = [];
+        if ($this->getMethod() === 'get') {
+            foreach ($_GET as $key => $value) {
+                $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        if ($this->getMethod() === 'post') {
+            foreach ($_POST as $key => $value) {
+                $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+
+        return $body;
+    }
+
+    public function setRouteParams($params)
+    {
+        $this->routeParams = $params;
+        return $this;
+    }
+    
+    public function getRouteParams()
+    {
+        return $this->routeParams;
+    }
+
+    public function getRouteParam($param, $default = null)
+    {
+        return $this->routeParams[$param] ?? $default;
+    }
+
+}
