7be4dc19c5ed211e71ed4cd269b71cc456ee2ed1
Jankes <jankes@jankes.com.pl>
2023-03-30 20:13:08 +0200

    Added Artist Model with get methods from Base Model

---
+++ app/Models/ArtistModel.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+
+class ArtistModel extends Model
+{
+    private int $artist_id;
+    private string $ksywa;
+    private string $imie;
+    private string $nazwisko;
+    private string $miasto;
+    private string $dob;
+    private string $bio;
+    private string $aka;
+    private string $strona;
+    private string $facebook;
+    private string $youtube;
+    private string $image;
+    private string $status;
+    private string $data_dod;
+    private string $datownik;
+
+    public static function tableName(): string 
+    {
+        return 'artist';
+    }
+
+    public static function primaryKey(): string 
+    {
+        return 'artist_id';
+    }
+    
+    public function getById(int $id)
+    {
+        $sql = "SELECT * FROM users WHERE id = :id LIMIT 1";
+        $params = ['id' => $id];
+        $result = $this->db->query($sql, $params);
+        return $result ? $result[0] : null;
+    }
+
+    public function getAll()//: object
+    {
+        return parent::findAll();
+    }
+
+    /*
+    public function create(array $data)
+    {
+        $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $params = [
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password']
+        ];
+        return $this->db->execute($sql, $params);
+    }
+    
+    public function update(int $id, array $data)
+    {
+        $sql = "UPDATE users SET name = :name, email = :email, password = :password WHERE id = :id";
+        $params = [
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password'],
+            'id' => $id
+        ];
+        return $this->db->execute($sql, $params);
+    }
+
+    public function delete(int $id)
+    {
+        $sql = "DELETE FROM users WHERE id = :id";
+        $params = ['id' => $id];
+        return $this->db->execute($sql, $params);
+    }
+    */
+}
