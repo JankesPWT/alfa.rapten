3d8352431594bcb97f69ec1ae791e501e5881c84
Jankes <jankes@jankes.com.pl>
2023-09-14 21:25:17 +0200

    Defined storage path in public/index

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use App\Core\App;
 
 /* CONSTANTS */
 define('VIEW_PATH', __DIR__ . '/../app/Views');
+define('STORAGE_PATH', __DIR__ . '/../storage');
 
 /* ROUTER */
 require_once __DIR__ . '/../app/routes.php';
